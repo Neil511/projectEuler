942341ced3fd13968c4c4423e2031d44a057bf4f
Neil Lohana <Neil_lohana@hotmail.com>
2016-04-10 14:07:18 -0400

    Sum of all Primes less than 2 million, updated isPrime predicate

--- 7.js
+++ 7.js
@@ -1,7 +1,7 @@
 // find 10001 prime number
 function isPrime(n){
   for(var i = 2; i <= Math.ceil(Math.sqrt(n)); i++){
-    if(n % i === 0){
+    if(n % i === 0 && n != i){
       return false;
     }
   }
@@ -10,7 +10,7 @@ function isPrime(n){
 
 function main(n){
   // find n'th prime number
-  var count = 1;
+  var count = 0;
   var curNum = 1;
 
   while(n != count){
@@ -21,5 +21,5 @@ function main(n){
   }
   return curNum;
 }
-
+console.log(isPrime(2));
 console.log(main(10001));
